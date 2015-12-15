f1618b0ad5e4d497c47474d963a9a8dc14b04b26
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-12-15 17:05:46 +0100

    Door1: Fix access to input file

--- door1/not-quite-lisp.rb
+++ door1/not-quite-lisp.rb
@@ -1,4 +1,4 @@
-input = IO.binread('./input.txt').chomp
+input = IO.binread(File.expand_path('../input.txt', __FILE__)).chomp
 floor_input = input.each_char.map { |c| c == '(' ? 1 : -1 }
 final_floor = floor_input.inject(0) { |sum, n| sum += n }
 puts "Santa has to go to floor: #{final_floor}"
