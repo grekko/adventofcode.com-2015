29c0dc40437b72054ffc4862f6c72388c2549147
Gregory Igelmund <gregory.igelmund@gmail.com>
2015-12-15 16:52:31 +0100

    Rename quiz-input.txt to input.txt

--- door1/not-quite-lisp.rb
+++ door1/not-quite-lisp.rb
@@ -1,4 +1,4 @@
-input = IO.binread('./quiz-input.txt').chomp
+input = IO.binread('./input.txt').chomp
 floor_input = input.each_char.map { |c| c == '(' ? 1 : -1 }
 final_floor = floor_input.inject(0) { |sum, n| sum += n }
 puts "Santa has to go to floor: #{final_floor}"
